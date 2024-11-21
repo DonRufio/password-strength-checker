475d8812bc82840df1638cc243a9044e0989e472
Raffaele Puzzuoli <raffaelepuzzuoli@gmail.com>
2024-11-21 20:51:00 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import zxcvbn from 'zxcvbn';
 
 function App() {
+  const [password, setPassword] = useState('');
+  const [result, setResult] = useState(null);
+
+  const handlePasswordChange = (event) => {
+    const password = event.target.value;
+    setPassword(password);
+
+    const feedback = zxcvbn(password);
+    setResult(feedback);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="password-checker">
+      <input type="password" value={password} onChange={handlePasswordChange} />
+      {result && (
+        <div>
+          <p>Strength: {result.score}</p>
+          <p>Estimated Cracking Time:</p>
+          <ul>
+            <li>Offline, slow hashing: {result.crack_times_display.offline_slow_hashing_1e4_per_second}</li>
+            <li>Offline, fast hashing: {result.crack_times_display.offline_fast_hashing_1e10_per_second}</li>
+            <li>Online cracking: {result.crack_times_display.online_no_throttling_100_per_second}</li>
+          </ul>
+          {/* Other feedback and suggestions */}
+        </div>
+      )}
     </div>
   );
 }
